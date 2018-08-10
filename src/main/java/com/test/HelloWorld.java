77a62ffed822d5bca6a0b45ecf838fc6e25b38c7
shawn <591174018@qq.com>
2018-08-10 16:26:31 +0800

    haha

---
+++ src/main/java/com/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.test;
+
+/**
+ * @Author： shawn
+ * @Description：
+ * @Date： Create in 16:16 2018/8/10
+ * @ModifiedBy:
+ */
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+}
