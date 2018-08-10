228d41967c2b913a56d159d4e02f1c6dbe14ad1e
shawn <591174018@qq.com>
2018-08-10 17:13:29 +0800

    haha

--- src/main/java/com/test/TestThird.java
+++ src/main/java/com/test/TestThird.java
@@ -9,4 +9,10 @@ package com.test;
 
 
 public class TestThird {
+
+    public static void main(String[] args) {
+        System.out.println("11111111master");
+        System.out.println("local");
+    }
+
 }
